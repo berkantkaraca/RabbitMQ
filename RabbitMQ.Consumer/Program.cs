af574b1fea782620ea5ca6bc08103f83f6f79d63
Berkant Karaca <berkantkaracatr@gmail.com>
2026-02-25 01:47:05 +0300

    advance producer-consumer example

--- RabbitMQ.Consumer/Program.cs
+++ RabbitMQ.Consumer/Program.cs
@@ -12,11 +12,19 @@ using IModel channel = connection.CreateModel();
 
 // 3- Queue Oluşturma
 // Consumer'daki kuyruk publisher'daki ile birebir aynı yapılandırmada tanımlanmalıdır.
-channel.QueueDeclare(queue: "example-queue", exclusive: false);
+channel.QueueDeclare(queue: "example-queue", exclusive: false, durable: true);
 
 // 4- Queue'dan Mesaj Okuma
 EventingBasicConsumer consumer = new(channel);
+
+// autoAck: false => Consume edilecek mesajın consumerdan bilgi gelmedikçe rabbitmq tarafından silinmeyeceği anlamına gelir. Yani mesajın başarılı bir şekilde işlendiği bilgisini consumerdan bekler. Eğer autoAck: true olursa, mesaj tüketildikten hemen sonra kuyruktan silinir, bu da mesaj kaybına yol açabilir.
 channel.BasicConsume(queue: "example-queue", autoAck: false, consumer: consumer);
+
+// Mesaj işleme konfigürasyonu
+// prefetchSize: 0 => Bir consumer tarafından alınabilecek en büyük mesaj boyutunu byte cinsinden belirler. 0, sınırsız demektir.
+// prefetchCount: 1 => Bir consumer tarafından aynı anda işlenebilecek mesaj sayısını belirler.
+// global: false => Bu ayar, prefetchCount ve prefetchSize ayarlarının sadece bu kanal için geçerli olduğunu belirtir. Eğer global: true olursa, bu ayarlar tüm kanallar için geçerli olur.
+channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
 
 consumer.Received += (sender, e) =>
 {
@@ -24,6 +32,14 @@ consumer.Received += (sender, e) =>
     //e.Body : Kuyruktaki mesajın verisini bütünsel olarak getirecektir.
     //e.Body.Span veya e.Body.ToArray() : Kuyrukdaki mesajın byte verisini getirecektir.
     Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+
+    // Mesajın başarılı bir şekilde işlendiği bilgisini RabbitMQ'ya bildirme
+    channel.BasicAck(deliveryTag: e.DeliveryTag, multiple: false);
+
+    // BasicNack : Mesajın işlenemediği bilgisini RabbitMQ'ya bildirme
+    // requeue: true => Mesajın tekrar kuyruğa atılmasını sağlar.
+    // requeue: false => Mesajın kuyruğa atılmadan silinmesini sağlar.
+    // channel.BasicNack(deliveryTag: e.DeliveryTag, multiple: false, requeue: true);
 };
 
 Console.Read();
--- RabbitMQ.Publisher/Program.cs
+++ RabbitMQ.Publisher/Program.cs
@@ -10,15 +10,21 @@ using IConnection connection = factory.CreateConnection();
 using IModel channel = connection.CreateModel();
 
 // 3- Queue Oluşturma
-channel.QueueDeclare(queue: "example-queue", exclusive: false);
+// durable: true => RabbitMQ'nun herhangi bir sebeple kapanması durumunda kuyrukta bulunan mesajların kaybolmaması için kullanılır. Eğer durable: false olursa, RabbitMQ kapanırsa kuyrukta bulunan mesajlar kaybolur.
+channel.QueueDeclare(queue: "example-queue", exclusive: false, durable: true);
 
 // 4- Queue'ya Mesaj Gönderme
 //RabbitMQ kuyruğa atacağı mesajları byte dizisi türünde kabul eder.
+
+// BasicProperties => publish edilen mesajların sunucu kapandığında kaybolmaması için mesajların kalıcı (persistent) olarak işaretlenmesini sağlar. Eğer mesajlar persistent olarak işaretlenmezse, RabbitMQ kapanırsa kuyrukta bulunan mesajlar kaybolur.
+IBasicProperties properties = channel.CreateBasicProperties();
+properties.Persistent = true;
+
 for (int i = 0; i < 100; i++)
 {
     await Task.Delay(200);
     byte[] message = Encoding.UTF8.GetBytes("Merhaba " + i);
-    channel.BasicPublish(exchange: "", routingKey: "example-queue", body: message);
+    channel.BasicPublish(exchange: "", routingKey: "example-queue", body: message, basicProperties: properties);
 }
 
 Console.Read();
