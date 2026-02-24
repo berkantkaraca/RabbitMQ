5dfa1554b5673831577949924b377b7d544a245c
Berkant Karaca <berkantkaracatr@gmail.com>
2026-02-24 10:54:37 +0300

    basic producer-consumer example

---
+++ RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,29 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+// 1- Bağlantı Oluşturma
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://LINK");
+
+// 2- Bağlantı Aktifleştirme ve Kanal Açma
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+// 3- Queue Oluşturma
+// Consumer'daki kuyruk publisher'daki ile birebir aynı yapılandırmada tanımlanmalıdır.
+channel.QueueDeclare(queue: "example-queue", exclusive: false);
+
+// 4- Queue'dan Mesaj Okuma
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue: "example-queue", autoAck: false, consumer: consumer);
+
+consumer.Received += (sender, e) =>
+{
+    //Kuyruğa gelen mesajın işlendiği yerdir!
+    //e.Body : Kuyruktaki mesajın verisini bütünsel olarak getirecektir.
+    //e.Body.Span veya e.Body.ToArray() : Kuyrukdaki mesajın byte verisini getirecektir.
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+};
+
+Console.Read();
---
+++ RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,24 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+// 1- Bağlantı Oluşturma
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://LINK");
+
+// 2- Bağlantıyı Aktifleştirme ve Kanal Açma
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+// 3- Queue Oluşturma
+channel.QueueDeclare(queue: "example-queue", exclusive: false);
+
+// 4- Queue'ya Mesaj Gönderme
+//RabbitMQ kuyruğa atacağı mesajları byte dizisi türünde kabul eder.
+for (int i = 0; i < 100; i++)
+{
+    await Task.Delay(200);
+    byte[] message = Encoding.UTF8.GetBytes("Merhaba " + i);
+    channel.BasicPublish(exchange: "", routingKey: "example-queue", body: message);
+}
+
+Console.Read();
