167ef0879ba360c259d55ece449d8e1e93a7e664
Berkant Karaca <berkantkaracatr@gmail.com>
2026-02-27 12:14:52 +0300

    work queue example

---
+++ RabbitMQ.WorkQueue.Consumer/Program.cs
@@ -0,0 +1,40 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+// Work Queue(İş Kuyruğu) Tasarımı​
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://LINK");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+string queueName = "example-work-queue";
+
+channel.QueueDeclare(
+    queue: queueName,
+    durable: false,
+    exclusive: false,
+    autoDelete: false
+);
+
+EventingBasicConsumer consumer = new(channel);
+
+channel.BasicConsume(
+    queue: queueName,
+    autoAck: true,
+    consumer: consumer
+);
+
+channel.BasicQos(
+    prefetchCount: 1,
+    prefetchSize: 0,
+    global: false
+);
+
+consumer.Received += (sender, e) =>
+{
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+};
+
+Console.Read();
---
+++ RabbitMQ.WorkQueue.Publisher/Program.cs
@@ -0,0 +1,33 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+// Work Queue(İş Kuyruğu) Tasarımı​
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://LINK");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+string queueName = "example-work-queue";
+
+channel.QueueDeclare(
+    queue: queueName,
+    durable: false,
+    exclusive: false,
+    autoDelete: false
+);
+
+for (int i = 0; i < 100; i++)
+{
+    await Task.Delay(200);
+
+    byte[] message = Encoding.UTF8.GetBytes("merhaba" + i);
+
+    channel.BasicPublish(
+        exchange: string.Empty,
+        routingKey: queueName,
+        body: message
+    );
+}
+
+Console.Read();
