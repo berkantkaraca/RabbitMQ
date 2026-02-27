e62d9a483e8b8e77feedf5b6fcf2a45aa8249169
Berkant Karaca <berkantkaracatr@gmail.com>
2026-02-27 11:43:56 +0300

    point-to-point example

---
+++ RabbitMQ.P2P.Consumer/Program.cs
@@ -0,0 +1,34 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+// P2P (Point-to-Point) Tasarımı
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://LINK");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+string queueName = "example-p2p-queue";
+
+channel.QueueDeclare(
+    queue: queueName,
+    durable: false,
+    exclusive: false,
+    autoDelete: false
+);
+
+EventingBasicConsumer consumer = new(channel);
+
+channel.BasicConsume(
+    queue: queueName,
+    autoAck: false,
+    consumer: consumer
+);
+
+consumer.Received += (sender, e) =>
+{
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+};
+
+Console.Read();
---
+++ RabbitMQ.P2P.Publisher/Program.cs
@@ -0,0 +1,28 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+// P2P (Point-to-Point) Tasarımı
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://LINK");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+string queueName = "example-p2p-queue";
+
+channel.QueueDeclare(
+    queue: queueName,
+    durable: false,
+    exclusive: false,
+    autoDelete: false
+ );
+
+byte[] message = Encoding.UTF8.GetBytes("merhaba");
+
+channel.BasicPublish(
+    exchange: string.Empty,
+    routingKey: queueName,
+    body: message
+);
+
+Console.Read();
