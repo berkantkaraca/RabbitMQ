6be51c406d4ef601e62199d61b3ec6b0a318392f
Berkant Karaca <berkantkaracatr@gmail.com>
2026-02-27 11:49:10 +0300

    publish subscribe example

---
+++ RabbitMQ.PublishSubscribe.Consumer/Program.cs
@@ -0,0 +1,44 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+// Publish/Subscribe (Pub/Sub) Tasarımı
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://LINK");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+string exchangeName = "example-pub-sub-exchange";
+
+channel.ExchangeDeclare(exchange: exchangeName, type: ExchangeType.Fanout);
+
+string queueName = channel.QueueDeclare().QueueName;
+
+channel.QueueBind(
+    queue: queueName,
+    exchange: exchangeName,
+    routingKey: string.Empty
+);
+
+// tüm consumer'lar o anda sadece 1 mesaj işleyebilir ve boyut sınırı yoktur.
+channel.BasicQos(
+    prefetchCount: 1,
+    prefetchSize: 0,
+    global: false
+);
+
+EventingBasicConsumer consumer = new(channel);
+
+channel.BasicConsume(
+    queue: queueName,
+    autoAck: false,
+    consumer: consumer
+);
+
+consumer.Received += (sender, e) =>
+{
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+};
+
+Console.Read();
---
+++ RabbitMQ.PublishSubscribe.Publisher/Program.cs
@@ -0,0 +1,28 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+// Publish/Subscribe (Pub/Sub) Tasarımı
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://LINK");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+string exchangeName = "example-pub-sub-exchange";
+
+channel.ExchangeDeclare(exchange: exchangeName, type: ExchangeType.Fanout);
+
+for (int i = 0; i < 100; i++)
+{
+    await Task.Delay(200);
+
+    byte[] message = Encoding.UTF8.GetBytes("merhaba" + i);
+
+    channel.BasicPublish(
+        exchange: exchangeName,
+        routingKey: string.Empty,
+        body: message
+    );
+}
+
+Console.Read();
