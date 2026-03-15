e56564ee8a015e66373ad5d51f505da30d3b4fc4
Berkant Karaca <berkantkaracatr@gmail.com>
2026-03-15 12:24:45 +0300

    masstransit basic example

---
+++ RabbitMQ.ESB.MassTransit.Consumer/Consumers/ExampleMessageConsumer.cs
@@ -0,0 +1,17 @@
+﻿using MassTransit;
+using RabbitMQ.ESB.MassTransit.Shared.Messages;
+
+namespace RabbitMQ.ESB.MassTransit.Consumer.Consumers
+{
+    /// <summary>
+    /// Kuyruğa IMessage türünde bir mesaj geldiğinde bu sınıf tarafından karşılanır ve Consume metodu çalıştırılır.
+    /// </summary>
+    public class ExampleMessageConsumer : IConsumer<IMessage>
+    {
+        public Task Consume(ConsumeContext<IMessage> context)
+        {
+            Console.WriteLine($"Gelen mesaj : {context.Message.Text}");
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ RabbitMQ.ESB.MassTransit.Shared/Messages/IMessage.cs
@@ -0,0 +1,10 @@
+﻿namespace RabbitMQ.ESB.MassTransit.Shared.Messages
+{
+    /// <summary>
+    /// Uygulamalardaki mesaj formatını belirler. (sipariş oluşturma, sipariş iptal, stok güncelleme vb.)
+    /// </summary>
+    public interface IMessage
+    {
+        public string Text { get; set; }
+    }
+}
